3c73c710215db2e7f8ef38ec4f90b06bd209e7b7
Anxhul10 <anxhul10@gmail.com>
2026-03-28 16:11:08 +0530

    call fn from lua

--- lua/main.c
+++ lua/main.c
@@ -12,12 +12,21 @@ int main(void) {
       return -1;// check lua started up or not
     }
     int status = luaL_loadfile(L, "hello.lua"); // load lua file
-    int ret = lua_pcall(L, 0, 0, 0); // tell lua to run script
+
+    int ret = lua_pcall(L, 0, 0, 0); // calll function
+    
     if (ret) {
         fprintf(stderr, "%s\n", lua_tostring(L, -1));
         return 1;
     }
 
+    lua_getglobal(L, "hello");     // push function
+    if (lua_pcall(L, 0, 1, 0) != 0) {
+        fprintf(stderr, "%s\n", lua_tostring(L, -1));
+        return 1;
+    }
+    const char *res = lua_tostring(L, -1);
+    printf("%s\n", res);
     lua_close(L);
     return 0;
 }
